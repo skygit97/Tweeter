8465661ed4f002ea9e2892a4041a478fb5e593e8
skumtong <sky_kumtong@yahoo.com>
2021-04-18 14:35:12 -0700

    Add login and register routes

--- app.js
+++ app.js
@@ -1,10 +1,31 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+const db = require("./config/keys").mongoURI;
+const users = require("./routes/api/users");
+const tweets = require("./routes/api/tweets");
+const User = require("./models/User");
 
+mongoose
+	.connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+	.then(() => console.log("Connected to mongoDB successfully"))
+	.catch((err) => console.log(err));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.get("/", (req, res) => {
+	const user = new User({
+		handle: "jim",
+		email: "jim@jim.jim",
+		password: "jimisgreat123",
+	});
+	user.save();
 	res.send("Hello World!");
 });
+
+app.use("/api/users", users);
+app.use("/api/tweets", tweets);
 
 const port = process.env.PORT || 5000;
 
