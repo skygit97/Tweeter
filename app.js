c56dfe85a585838dfcf98dadad8595c8277c3154
skumtong <sky_kumtong@yahoo.com>
2021-04-19 09:04:04 -0700

    Finish

--- app.js
+++ app.js
@@ -1,34 +1,32 @@
+const mongoose = require("mongoose");
 const express = require("express");
 const app = express();
-const mongoose = require("mongoose");
 const db = require("./config/keys").mongoURI;
 const users = require("./routes/api/users");
 const tweets = require("./routes/api/tweets");
-const User = require("./models/User");
+const bodyParser = require("body-parser");
+const passport = require("passport");
+
+const path = require("path");
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static("frontend/build"));
+	app.get("/", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+	});
+}
 
 mongoose
-	.connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
-	.then(() => console.log("Connected to mongoDB successfully"))
+	.connect(db, { useNewUrlParser: true })
+	.then(() => console.log("Connected to MongoDB successfully"))
 	.catch((err) => console.log(err));
+
+app.use(passport.initialize());
+
+require("./config/passport")(passport);
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.get("/", (req, res) => {
-	const user = new User({
-		handle: "jim",
-		email: "jim@jim.jim",
-		password: "jimisgreat123",
-	});
-	user.save();
-	res.send("Hello World!");
-});
-
-app.use("/api/users", users);
-app.use("/api/tweets", tweets);
-
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => {
-	console.log(`Listening on port ${port}`);
-});
+app.listen(port, () => console.log(`Server is running on port ${port}`));
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,20 +1,27 @@
 const express = require("express");
 const router = express.Router();
-const User = require("../../models/User");
 const bcrypt = require("bcryptjs");
-const keys = require("../../config/keys");
+const User = require("../../models/User");
 const jwt = require("jsonwebtoken");
+const keys = require("../../config/keys");
+const passport = require("passport");
+const validateRegisterInput = require("../../validation/register");
+const validateLoginInput = require("../../validation/login");
 
-router.get("/test", (req, res) => {
-	res.json({ msg: "This is the user route" });
-});
+router.get("/test", (req, res) => res.json({ msg: "This is the users route" }));
 
 router.post("/register", (req, res) => {
+	const { errors, isValid } = validateRegisterInput(req.body);
+
+	if (!isValid) {
+		return res.status(400).json(errors);
+	}
+
 	User.findOne({ email: req.body.email }).then((user) => {
 		if (user) {
 			return res
 				.status(400)
-				.json({ email: "A user is already registered with that email" });
+				.json({ email: "A user has already registered with this address" });
 		} else {
 			const newUser = new User({
 				handle: req.body.handle,
@@ -28,7 +35,21 @@ router.post("/register", (req, res) => {
 					newUser.password = hash;
 					newUser
 						.save()
-						.then((user) => res.json(user))
+						.then((user) => {
+							const payload = { id: user.id, handle: user.handle };
+
+							jwt.sign(
+								payload,
+								keys.secretOrKey,
+								{ expiresIn: 3600 },
+								(err, token) => {
+									res.json({
+										success: true,
+										token: "Bearer " + token,
+									});
+								}
+							);
+						})
 						.catch((err) => console.log(err));
 				});
 			});
@@ -37,6 +58,11 @@ router.post("/register", (req, res) => {
 });
 
 router.post("/login", (req, res) => {
+	const { errors, isValid } = validateLoginInput(req.body);
+
+	if (!isValid) {
+		return res.status(400).json(errors);
+	}
 	const email = req.body.email;
 	const password = req.body.password;
 
@@ -47,14 +73,11 @@ router.post("/login", (req, res) => {
 
 		bcrypt.compare(password, user.password).then((isMatch) => {
 			if (isMatch) {
-				const payload = {
-					id: user.id,
-					handle: user.handle,
-					email: user.email,
-				};
+				const payload = { id: user.id, handle: user.handle };
 				jwt.sign(
 					payload,
 					keys.secretOrKey,
+					// Tell the key to expire in one hour
 					{ expiresIn: 3600 },
 					(err, token) => {
 						res.json({
@@ -70,25 +93,16 @@ router.post("/login", (req, res) => {
 	});
 });
 
-// bcrypt.compare(password, user.password).then((isMatch) => {
-// 	if (isMatch) {
-// 		const payload = { id: user.id, handle: user.handle };
-
-// 		jwt.sign(
-// 			payload,
-// 			keys.secretOrKey,
-// 			// Tell the key to expire in one hour
-// 			{ expiresIn: 3600 },
-// 			(err, token) => {
-// 				res.json({
-// 					success: true,
-// 					token: "Bearer " + token,
-// 				});
-// 			}
-// 		);
-// 	} else {
-// 		return res.status(400).json({ password: "Incorrect password" });
-// 	}
-// });
+router.get(
+	"/current",
+	passport.authenticate("jwt", { session: false }),
+	(req, res) => {
+		res.json({
+			id: req.user.id,
+			handle: req.user.handle,
+			email: req.user.email,
+		});
+	}
+);
 
 module.exports = router;
